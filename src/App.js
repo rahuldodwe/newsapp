6152b52404df12b391ec25a7c5e723133c2f565c
Rahul Dodwe <rahuldodwe7747909605@gmail.com>
2024-03-05 09:53:54 +0530

    NewsApp Update-01

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
-  c ='Comp01'
+
   render() {
     return (
       <div>
-        Hello my first class based component: {this.c}
+        <NavBar></NavBar>
+        <News></News>
       </div>
     )
   }
