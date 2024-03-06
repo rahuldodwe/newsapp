8009b3079513c9255797996ae05119fc21e8784f
Rahul Dodwe <rahuldodwe7747909605@gmail.com>
2024-03-06 21:50:57 +0530

    update-02(1)

--- src/components/News.js
+++ src/components/News.js
@@ -41,20 +41,15 @@ export class News extends Component {
     return (
       <div className="container my-3" >
         <h2>NewsApp - Top headlines</h2>
-
+        
         <div className="row">
-          <div className="col-md-4">
-            <NewsItem title="myTitle" description="mydesc" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg" newsUrl="TODO" />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="myTitle" description="mydesc" />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="myTitle" description="mydesc" />
-          </div>
+        {this.state.articles.map((element)=>{
+          return <div className="col-md-4" key={element.url} >
+          <NewsItem key={element.url} title={element.title.slice(0, 26)} description={element.description.slice(0, 85)} imageUrl={element.urlToImage} newsUrl={element.url} />
+        </div> 
+        })}
+                 
         </div>
-
-
       </div>
     )
   }
