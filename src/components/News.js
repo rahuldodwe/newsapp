dadf7df2bae41decdc580ecbc2f3b17a01c36886
Rahul Dodwe <rahuldodwe7747909605@gmail.com>
2024-03-12 22:53:10 +0530

    update04

--- src/components/News.js
+++ src/components/News.js
@@ -10,14 +10,36 @@ export class News extends Component {
     }   
 }
   async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=df705ef9df674b269c4be2cb984cbecd";
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=df705ef9df674b269c4be2cb984cbecd&page=1";
     let data = await fetch(url);
     let parsedData = await data.json()
     console.log(parsedData);
     this.setState({articles: parsedData.articles})
   }
 
-  
+  handlePrevClick = async ()=> {
+    console.log("Previous");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=df705ef9df674b269c4be2cb984cbecd&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles})
+  }
+
+  handleNextClick = async ()=> {
+    console.log("Next");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=df705ef9df674b269c4be2cb984cbecd&page=${this.state.page + 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles})
+  }
+
+ 
 
   render() {
     return (
@@ -32,7 +54,7 @@ export class News extends Component {
         })}                 
         </div>
         <div className="container d-flex justify-content-between">
-        <button type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+        <button disabled={this.state.page=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
         <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
